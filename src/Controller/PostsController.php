a54f2da6375f6750672d01c79825a62f23a6ac5c
odziminski <o.dziminski@gmail.com>
2022-12-28 19:55:29 +0100

    add single post subpage, add post deletion option

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -4,10 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class PostsController extends AbstractController
 {
@@ -19,6 +19,28 @@ class PostsController extends AbstractController
         $posts = $em->getRepository(Post::class)->findBy([], ['id' => 'ASC']);
         return $this->render('posts/index.html.twig', [
             'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/post/delete/{id}", name="post_delete", methods={"POST"})
+     */
+    public function delete(Post $post): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('lista');
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_show", methods={"GET"})
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('posts/show.html.twig', [
+            'post' => $post,
         ]);
     }
 
