d3c3e0cce62c8b5bf40e17a749cf1486611d818c
odziminski <o.dziminski@gmail.com>
2022-12-28 19:05:36 +0100

    add registration, add post data on frontend

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,13 @@ class PostsController extends AbstractController
     /**
      * @Route("/posts", name="app_posts")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $posts = $em->getRepository(Post::class)->findBy([], ['id' => 'ASC']);
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
+
+
 }
